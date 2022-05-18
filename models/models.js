ee099e942e781edb464ad88ff7e1378474a7815c
Gugandeep Singh Binning <gugandeep.s@hotmail.com>
2022-05-18 12:03:17 +0100

    Completed_Patch_Task

--- models/models.js
+++ models/models.js
@@ -18,3 +18,17 @@ exports.fetchReviewID = (reviewId) => {
         return review.rows[0];
       });
   };
+
+  exports.updatePatchReview = (votes, reviewId) => {
+      return db
+        .query(
+          `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *`,
+          [votes, reviewId]
+        )
+        .then((review) => {
+          if (review.rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "not found" });
+          }
+          return review.rows[0];
+        });
+    };
