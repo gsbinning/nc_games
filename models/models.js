c1943beda4729156e2c7cca13c561b9036b16625
Gugandeep Singh Binning <gugandeep.s@hotmail.com>
2022-06-08 22:40:43 +0100

    Task_delete_comment_complete

--- models/models.js
+++ models/models.js
@@ -92,3 +92,28 @@ exports.fetchReviewID = (reviewId) => {
 
     }
 
+    // exports.removeComment = (comment_id) => {
+    //   return db
+    //     .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`, [
+    //       comment_id,
+    //     ])
+    //     .then(({ rows }) => {
+    //       if (!rows.length) {
+    //         return Promise.reject({
+    //           status: 404,
+    //           message: "not found",
+    //         });
+    //       }
+    //     });
+    // };
+
+
+    exports.removeComment = (comment_id) => {
+      return db.query('DELETE FROM comments WHERE comment_id = $1 RETURNING *;', [comment_id])
+      .then((result) => {
+          if(!result.rows.length) {
+              return Promise.reject({ status: 404, msg: 'not found'})
+          }
+          return result.rows[0];
+      })
+  };
